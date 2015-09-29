9f2428311e0b71f899215cb2c57f4bc44c0e4904
quantum0813 <quantum0813@gmail.com>
2015-09-28 22:48:33 -0400

    Created repository

---
+++ esv5-HW1-2.sql
@@ -0,0 +1,37 @@
+INSERT INTO Products VALUES('A', 1001, 'pc');
+INSERT INTO Products VALUES('A', 1002, 'pc');
+INSERT INTO Products VALUES('A', 1003, 'pc');
+INSERT INTO Products VALUES('A', 2004, 'laptop');
+INSERT INTO Products VALUES('A', 2005, 'laptop');
+INSERT INTO Products VALUES('A', 2006, 'laptop');
+
+INSERT INTO Products VALUES('B', 1004, 'pc');
+INSERT INTO Products VALUES('B', 1005, 'pc');
+INSERT INTO Products VALUES('B', 1006, 'pc');
+INSERT INTO Products VALUES('B', 2007, 'laptop');
+
+INSERT INTO Products VALUES('C', 1007, 'pc');
+
+INSERT INTO Products VALUES('D', 1008, 'pc');
+INSERT INTO Products VALUES('D', 1009, 'pc');
+INSERT INTO Products VALUES('D', 1010, 'pc');
+INSERT INTO Products VALUES('D', 3004, 'printer');
+INSERT INTO Products VALUES('D', 3005, 'printer');
+
+INSERT INTO Products VALUES('E', 1011, 'pc');
+INSERT INTO Products VALUES('E', 1012, 'pc');
+INSERT INTO Products VALUES('E', 1013, 'pc');
+INSERT INTO Products VALUES('E', 2001, 'laptop');
+INSERT INTO Products VALUES('E', 2002, 'laptop');
+INSERT INTO Products VALUES('E', 2003, 'laptop');
+INSERT INTO Products VALUES('E', 3001, 'printer');
+INSERT INTO Products VALUES('E', 3002, 'printer');
+INSERT INTO Products VALUES('E', 3003, 'printer');
+
+INSERT INTO Products VALUES('F', 2008, 'laptop');
+INSERT INTO Products VALUES('F', 2009, 'laptop');
+
+INSERT INTO Products VALUES('G', 2010, 'laptop');
+
+INSERT INTO Products VALUES('H', 3006, 'printer');
+INSERT INTO Products VALUES('H', 3007, 'printer');
